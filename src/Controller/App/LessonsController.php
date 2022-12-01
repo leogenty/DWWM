bbf49701cc1b6cfdf9449bf095278833e38312ca
leogenty <l.genty@cs2i-bourgogne.com>
2022-12-01 15:57:33 +0100

    add matter page (index, sass, js, controller, navbar link). matter page displays list of categories with matters in each category.
    Signed-off-by: leogenty <l.genty@cs2i-bourgogne.com>

--- src/Controller/App/LessonsController.php
+++ src/Controller/App/LessonsController.php
@@ -2,15 +2,23 @@
 
 namespace App\Controller\App;
 
+use App\Entity\Chapter;
+use App\Entity\Lesson;
+use App\Entity\Matter;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class LessonsController extends AbstractController
 {
-    #[Route('/app/lessons', name: 'app_lessons')]
-    public function index(): Response
+    #[Route('app/lessons/{title}', name: 'app_lessons')]
+    public function index(Request $request, ManagerRegistry $managerRegistry): Response
     {
-        return $this->render('app/pages/lessons/index.html.twig');
+        return $this->render('app/pages/lessons/index.html.twig', [
+            'chapters' => $managerRegistry->getRepository(Chapter::class)->findBy(['matter' => $managerRegistry->getRepository(Matter::class)->findOneBy(['name' => $request->get('title')])]),
+            'lessons' => $managerRegistry->getRepository(Lesson::class)->findAll(),
+        ]);
     }
 }
