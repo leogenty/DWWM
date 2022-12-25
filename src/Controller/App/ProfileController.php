4feb94bcca928907a3b9f96c0b323c298ea6e7f4
leogenty <l.genty@cs2i-bourgogne.com>
2022-12-25 12:59:35 +0100

    made _progression tab in profile. add reset progression button. change complete math function.
    Signed-off-by: leogenty <l.genty@cs2i-bourgogne.com>

--- src/Controller/App/ProfileController.php
+++ src/Controller/App/ProfileController.php
@@ -5,6 +5,7 @@ namespace App\Controller\App;
 use App\Entity\Progression;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,6 +14,21 @@ class ProfileController extends AbstractController
     #[Route('/app/profile', name: 'app_profile')]
     public function index(ManagerRegistry $managerRegistry): Response
     {
+        return $this->render('app/pages/profile/index.html.twig', [
+            'progressions' => $managerRegistry->getRepository(Progression::class)->findAll(),
+        ]);
+    }
+
+    #[Route('/app/reset-progression', name: 'app_reset_progression')]
+    public function resetProgression(ManagerRegistry $managerRegistry, Request $request): Response
+    {
+        $progressionId = $request->get('progressionId');
+
+        $progression = $managerRegistry->getRepository(Progression::class)->find($progressionId);
+        $progression->setComplete(0);
+        $managerRegistry->getManager()->persist($progression);
+        $managerRegistry->getManager()->flush();
+
         return $this->render('app/pages/profile/index.html.twig', [
             'progressions' => $managerRegistry->getRepository(Progression::class)->findAll(),
         ]);
