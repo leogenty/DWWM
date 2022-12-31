c509f9cc74b54794a4d94942e228595a2daca6ce
Léo Genty <l.genty@cs2i-bourgogne.com>
2022-12-31 15:09:37 +0100

    finally fix progression can't go down. fix the bug where you cannot access to matter pages while not connected to an account. remove footer for security pages.
    Signed-off-by: leogenty <l.genty@cs2i-bourgogne.com>

--- src/Controller/App/LessonsController.php
+++ src/Controller/App/LessonsController.php
@@ -10,6 +10,7 @@ use App\Entity\Progression;
 use App\Entity\Type;
 use App\Form\AddProgressionType;
 use Doctrine\Persistence\ManagerRegistry;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -32,7 +33,7 @@ class LessonsController extends AbstractController
         foreach ($types as $type) {
             $progressionId = $request->get('progressionId');
 
-            if (null === $progressionId) {
+            if (null === $progressionId) {  // create progression table foreach types and set complete to 0
                 $progression = new Progression();
                 $form = $this->createForm(AddProgressionType::class, $progression, ['typeId' => $type]);
                 $form->handleRequest($request);
@@ -55,7 +56,7 @@ class LessonsController extends AbstractController
     }
 
     #[Route('app/lessons/{matter}/{typeNb}/{chapterNb}/{lessonNb}', name: 'app_lesson_single')]
-    public function single(Request $request, ManagerRegistry $managerRegistry): Response
+    public function single(Request $request, ManagerRegistry $managerRegistry, LoggerInterface $logger): Response
     {
         $lessonId = $request->get('lessonId');
         $typeId = $request->get('typeId');
@@ -69,17 +70,13 @@ class LessonsController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $progression->setComplete($form->get('complete')->getData());
-
-            $managerRegistry->getManager()->persist($progression);
-            $managerRegistry->getManager()->flush();
-
             if ($progression->getComplete() > $clonedProgression->getComplete()) { // if new progression > to the current one
-                $managerRegistry->getManager()->persist($progression);
-                $managerRegistry->getManager()->flush();
+                $progression->setComplete($form->get('complete')->getData());
             } else {
                 $progression->setComplete($clonedProgression->getComplete());
             }
+            $managerRegistry->getManager()->persist($progression);
+            $managerRegistry->getManager()->flush();
 
             return $this->redirectToRoute('app_lessons', ['matter' => $request->get('matter'), 'progressionId' => $progressionId]);
         }
