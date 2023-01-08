1ba32b2ba9aa93c404ac9c060969ca62ffb06e7d
Léo Genty <l.genty@cs2i-bourgogne.com>
2023-01-08 14:52:05 +0100

    add animation on scroll.
    Signed-off-by: leogenty <l.genty@cs2i-bourgogne.com>

--- assets/javascripts/front/pages/home/index.js
+++ assets/javascripts/front/pages/home/index.js
@@ -13,3 +13,62 @@ import 'bootstrap/js/dist/tab'
 import '../../../../styles/front/pages/home/index.scss'
 
 require('animate.css/animate.css')
+
+// animate on scroll
+const animatedElement1 = document.querySelectorAll('.element-to-fadeInRight');
+const animatedElement2 = document.querySelectorAll('.element-to-fadeInUp');
+
+function checkIfInView() {
+    const windowHeight = window.innerHeight;
+    const windowTopPosition = window.scrollY;
+    const windowBottomPosition = (windowTopPosition + windowHeight);
+
+    animatedElement1.forEach(el => {
+        const elementHeight = el.offsetHeight;
+        const elementTopPosition = el.offsetTop;
+        const elementBottomPosition = (elementTopPosition + elementHeight);
+
+        // check if this current element is within viewport
+        if ((elementBottomPosition >= windowTopPosition) &&
+            (elementTopPosition <= windowBottomPosition)) {
+            el.classList.add('animate__fadeInRight');
+        }
+    });
+    animatedElement2.forEach(el => {
+        const elementHeight = el.offsetHeight;
+        const elementTopPosition = el.offsetTop;
+        const elementBottomPosition = (elementTopPosition + elementHeight);
+
+        // check if this current element is within viewport
+        if ((elementBottomPosition >= windowTopPosition) &&
+            (elementTopPosition <= windowBottomPosition)) {
+            el.classList.add('animate__fadeInUp');
+        }
+    });
+}
+
+/* const animate = document.querySelectorAll('[class*=animate__]');
+console.log(animate);
+
+function checkIfInView() {
+    const windowHeight = window.innerHeight;
+    const windowTopPosition = window.scrollY;
+    const windowBottomPosition = (windowTopPosition + windowHeight);
+
+    animate.forEach(el => {
+        let className = el.getElementsByClassName();
+        console.log(className)
+
+        const elementHeight = el.offsetHeight;
+        const elementTopPosition = el.offsetTop;
+        const elementBottomPosition = (elementTopPosition + elementHeight);
+
+        // check if this current element is within viewport
+        if ((elementBottomPosition >= windowTopPosition) &&
+            (elementTopPosition <= windowBottomPosition)) {
+            el.classList.add('animate__fadeInUp');
+        }
+    });
+} */
+
+window.addEventListener('scroll', checkIfInView);
